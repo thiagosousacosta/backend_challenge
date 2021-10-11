57b42e50476fb114febe87a485ccf89e95fad6eb
thiagosousacosta <thiagodesousacosta@outlook.com>
2021-10-11 17:19:53 -0300

    Change in CarSerializer fields
    Change in car return structure, to return full wheel status

--- solution/api/carmaintenance/serializers.py
+++ solution/api/carmaintenance/serializers.py
@@ -6,11 +6,11 @@ from .models import Car, Tyre
 class TyreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tyre
-        fields = ['tyre_id', 'degradation', 'car_id']
+        fields = ['tyre_id', 'degradation', 'car_id', 'in_use']
 
 
 class CarSerializer(serializers.ModelSerializer):
-    tyres = serializers.StringRelatedField(many=True)
+    tyres = TyreSerializer(many=True, read_only=True)
 
     class Meta:
         model = Car
