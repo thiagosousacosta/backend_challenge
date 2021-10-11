56c937f6d1f7787356f253a78e8ff8da5848bbd2
thiagosousacosta <thiagodesousacosta@outlook.com>
2021-10-11 17:20:58 -0300

    File split
    Changing url addition to urls.py file

--- solution/api/api/urls.py
+++ solution/api/api/urls.py
@@ -1,5 +1,4 @@
-from carmaintenance.routes import router
-from carmaintenance.views import refuel, maintenance
+from carmaintenance.routes import function_urls, router
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,6 +6,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('refuel/', refuel, name='refuel'),
-    path('maintenance/', maintenance, name='maintenance'),
 ]
+
+urlpatterns.extend(function_urls)
--- solution/api/carmaintenance/routes.py
+++ solution/api/carmaintenance/routes.py
@@ -1,7 +1,13 @@
+from django.urls import path
 from rest_framework import routers
 
-from carmaintenance.views import CarViewSet, TyreViewSet
+from carmaintenance.views import CarViewSet, TyreViewSet, maintenance, refuel
 
 router = routers.DefaultRouter()
 router.register('cars', CarViewSet, basename='cars')
 router.register('tyres', TyreViewSet, basename='tyres')
+
+function_urls = [
+    path('refuel/', refuel, name='refuel'),
+    path('maintenance/', maintenance, name='maintenance'),
+]
